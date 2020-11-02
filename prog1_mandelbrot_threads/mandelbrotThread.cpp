63981166f38603e98d09addd528a53e96b671464
chenxfeng <chenxf47@mail2.sysu.edu.cn>
2020-11-02 12:29:46 +0800

    Add files via upload
    finish first program

--- prog1_mandelbrot_threads/mandelbrotThread.cpp
+++ prog1_mandelbrot_threads/mandelbrotThread.cpp
@@ -23,6 +23,24 @@ extern void mandelbrotSerial(
     int output[]);
 
 
+static inline int mandel(float c_re, float c_im, int count)
+{
+    float z_re = c_re, z_im = c_im;
+    int i;
+    for (i = 0; i < count; ++i) {
+
+        if (z_re * z_re + z_im * z_im > 4.f)
+            break;
+
+        float new_re = z_re*z_re - z_im*z_im;
+        float new_im = 2.f * z_re * z_im;
+        z_re = c_re + new_re;
+        z_im = c_im + new_im;
+    }
+
+    return i;
+}
+
 //
 // workerThreadStart --
 //
@@ -32,6 +50,25 @@ void* workerThreadStart(void* threadArgs) {
     WorkerArgs* args = static_cast<WorkerArgs*>(threadArgs);
 
     // TODO: Implement worker thread here.
+    float dx = (args->x1 - args->x0) / args->width;
+    float dy = (args->y1 - args->y0) / args->height;
+
+    int startRow = args->threadId * args->height / args->numThreads;
+    int endRow = startRow + args->height / args->numThreads;
+
+    for (int j = startRow; j < endRow; j++) {
+        for (int i = 0; i < args->width; ++i) {
+            float x = args->x0 + i * dx;
+            float y = args->y0 + j * dy;
+
+            int index = (j * args->width + i);
+            args->output[index] = mandel(x, y, args->maxIterations);
+
+            // if (j == 401 && i == 214) {
+            //   printf("%.10f %.10f %d %d\n", x, y, args->maxIterations, args->output[index]);
+            // }
+        }
+    }
 
     printf("Hello world from thread %d\n", args->threadId);
 
@@ -63,6 +100,15 @@ void mandelbrotThread(
     for (int i=0; i<numThreads; i++) {
         // TODO: Set thread arguments here.
         args[i].threadId = i;
+        args[i].x0 = x0;///task division should be in thread 
+        args[i].y0 = y0;/// to avoid numeric accuracy loss
+        args[i].x1 = x1;
+        args[i].y1 = y1;
+        args[i].width = width;
+        args[i].height = height;
+        args[i].maxIterations = maxIterations;
+        args[i].output = output;
+        args[i].numThreads = numThreads;
     }
 
     // Fire up the worker threads.  Note that numThreads-1 pthreads
